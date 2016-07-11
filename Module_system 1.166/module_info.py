75a8b5adde14dd72df1dc067deaa576dd3b3e4ac
OmerTheGreat <ruzgarinadi@yandex.com>
2016-07-11 22:26:05 +0300

    Add files via upload

---
+++ Module_system 1.166/module_info.py
@@ -0,0 +1,19 @@
+# Point export_dir to the folder you will be keeping your module
+# Make sure you use forward slashes (/) and NOT backward slashes (\)
+
+
+
+# Several possible variants for export_dir variable:
+
+# Warband being installed to C:/Games
+export_dir = "C:/Program Files (x86)/Steam/steamapps/common/MountBlade Warband/Modules/Kader-Mini/"
+
+# Warband being installed to default path on Windows XP or Vista
+#export_dir = "C:/Program Files (x86)/Steam/steamapps/common/MountBlade Warband/Modules/Kader-Mini/"
+
+# Warband being installed to default path on Windows 7+
+#export_dir = "C:/Program Files (x86)/Steam/steamapps/common/MountBlade Warband/Modules/Kader-Mini/"
+
+# Likely paths for Steam Warband installation:
+#export_dir = "C:/Program Files (x86)/Steam/steamapps/common/MountBlade Warband/Modules/Kader-Mini/"
+#export_dir = "C:/Program Files (x86)/Steam/steamapps/common/MountBlade Warband/Modules/Kader-Mini/"
